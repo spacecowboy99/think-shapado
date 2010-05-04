48f897ec41205003519a529b602b2c280c39d799
~spacecowboy <simon.probert1@googlemail.com>
2010-05-05 00:15:41 +1000

    update

--- scripts/m-restore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Script to backup database
-#
-
-echo "Restore started"
-sudo ~/mongodb/bin/mongorestore -d shapado-development  backup/shapado-development --drop
-sudo ~/mongodb/bin/mongorestore -d shapado-development  backup/shapado-development
-echo "Restore completed"
