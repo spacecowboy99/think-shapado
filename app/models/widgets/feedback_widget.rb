01b82dcac3b52245bd1ff984d1a1841886d01b67
~spacecowboy <simon.probert1@googlemail.com>
2010-05-05 13:55:06 +1000

    Added the feedback widget

--- app/models/widgets/feedback_widget.rb
+++ app/models/widgets/feedback_widget.rb
@@ -1,8 +1,12 @@
 class FeedbackWidget < Widget
   before_validation_on_create :set_name
 
+  def local_version
+    @local_version = AppConfig.version
+  end
+
   protected
   def set_name
-    self[:name] ||= "Feedback"
+    self[:name] ||= "feedback"
   end
 end
